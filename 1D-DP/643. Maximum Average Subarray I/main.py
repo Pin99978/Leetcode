a21ed1ecb7ac535e1c1961c6a13618136b47ab45
Pin Pin <99624960+Pin99978@users.noreply.github.com>
2024-01-20 21:46:38 +0800

    Create main.py

---
+++ 1D-DP/643. Maximum Average Subarray I/main.py
@@ -0,0 +1,22 @@
+# Time-Consume : 874 Space : >56.11 
+# O(n)                 
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+
+        if len(nums) ==k:
+            return sum(nums) / k
+               
+        pt1 = 0
+        pt2 = k
+        # get the first max result
+        max_sum = sum(nums[pt1 : pt2])
+        cur_sum = max_sum
+        while pt2 < len(nums) :
+          
+            cur_sum = cur_sum - nums[pt1] + nums[pt2]
+            if ( cur_sum > max_sum ):
+                max_sum = cur_sum
+            pt2+=1
+            pt1+=1 
+        return max_sum / k
