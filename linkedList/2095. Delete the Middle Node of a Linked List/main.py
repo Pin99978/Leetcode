688e0724e6a480d4cd45d321f8e4aae62999e92a
Pin Pin <99624960+Pin99978@users.noreply.github.com>
2024-02-03 15:47:58 +0800

    Create main.py
    Loop first -> find middle -> Loop

---
+++ linkedList/2095. Delete the Middle Node of a Linked List/main.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+
+        # if head.next = None
+        
+        if head.next ==None or head ==None:
+            
+            return  None
+
+        # go through the full node and determine the length
+        i = 0
+        tmp = head       
+        while  head != None :
+            
+            head = head.next
+            i += 1 
+        
+        middle = i // 2         
+        start = 0 
+        # move head back to inital node
+        head = tmp
+
+        
+        # move the node to the middle - 1 node  
+        while  start < middle - 1  :
+            tmp = tmp.next
+            start +=1
+            
+        #now start at the head -1 node
+        tmp.next =tmp.next.next
+        
+        return head
+        
